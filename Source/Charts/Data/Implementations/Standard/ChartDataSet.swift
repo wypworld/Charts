83acfd1c54e98fc6bcf6ddeebbbd9f79b7938685
wypworld <wangyunpeng_pro@163.com>
2023-02-01 11:52:10 +0800

    Update ChartDataSet.swift
    解决Xcode 14编译错误

--- Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
+++ Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
@@ -565,6 +565,11 @@ extension ChartDataSet: RangeReplaceableCollection {
         entries.removeSubrange(bounds)
         notifyDataSetChanged()
     }
+    
+    public func replaceSubrange<R,C>(_ subrange: R, with newElements: C) where R : RangeExpression, Index == R.Bound, C : Collection, ChartDataEntry == C.Element {
+        entries.replaceSubrange(subrange, with: newElements)
+        notifyDataSetChanged()
+    }
 
     @objc
     public func removeAll(keepingCapacity keepCapacity: Bool) {
